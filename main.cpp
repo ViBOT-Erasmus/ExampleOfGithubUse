b478c36ed810ce0ec227f7fa603b304507c86e64
Cedric Lemaitre <c.lemaitre58@gmail.com>
2020-04-05 17:31:59 +0200

    new version of main.cpp in branch dev-cedlemaitre

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@
 
 int main()
 {
-    std::cout << "Example of Git and Github use" << std::endl;
+    std::cout << "Example of Git and Github use in branch dev-cedlemaitre" << std::endl;
     return 0;
 }
